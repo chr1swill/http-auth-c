ff26417928b17ebaaac1aa81eb830fa60631b135
chr1swill <alexanderwilliams20003@gmail.com>
2025-06-30 16:16:57 -0400

    fix: ensure correct sizeof expression is used on string when comparing - when you do a sizeof on a null terminated string it counts the invisible '\0' as a byte as well. Duh!

--- src/main.c
+++ src/main.c
@@ -290,23 +290,23 @@ int pollfd_add(nfds_t *nfds, struct pollfd *pfds, int connfd, int events)
 static inline
 enum http_method as_http_method(const char *method)
 {
-  if (memcmp(method, "GET", sizeof "GET") == 0) {
+  if (memcmp(method, "GET", sizeof("GET") - 1) == 0) {
     return http_method_get;
-  } else if (memcmp(method, "HEAD", sizeof "HEAD") == 0) {
+  } else if (memcmp(method, "HEAD", sizeof("HEAD") - 1) == 0) {
     return http_method_head;
-  } else if (memcmp(method, "POST", sizeof "POST") == 0) {
+  } else if (memcmp(method, "POST", sizeof("POST") - 1) == 0) {
     return http_method_post;
-  } else if (memcmp(method, "PUT", sizeof "PUT") == 0) {
+  } else if (memcmp(method, "PUT", sizeof("PUT") - 1) == 0) {
     return http_method_put;
-  } else if (memcmp(method, "PATCH", sizeof "PATCH") == 0) {
+  } else if (memcmp(method, "PATCH", sizeof("PATCH") - 1 ) == 0) {
     return http_method_patch;
-  } else if (memcmp(method, "DELETE", sizeof "DELETE") == 0) {
+  } else if (memcmp(method, "DELETE", sizeof("DELETE") - 1) == 0) {
     return http_method_delete;
-  } else if (memcmp(method, "CONNECT", sizeof "CONNECT") == 0) {
+  } else if (memcmp(method, "CONNECT", sizeof("CONNECT") - 1) == 0) {
     return http_method_connect;
-  } else if (memcmp(method, "OPTION", sizeof "OPTION") == 0) {
+  } else if (memcmp(method, "OPTION", sizeof("OPTION") - 1) == 0) {
     return http_method_option;
-  } else if (memcmp(method, "TRACE", sizeof "TRACE") == 0) {
+  } else if (memcmp(method, "TRACE", sizeof("TRACE") - 1) == 0) {
     return http_method_trace;
   } else {
     return http_method_invalid_method;
@@ -464,7 +464,7 @@ int main()
               pfds[i].revents = -1;
               continue;
             }
-              
+
             if (!http_method_is(http_method_get, php_method[client_idx()])) {
               client_status_code[client_idx()] = http_status_methodnotallowed;
               pfds[i].events = POLLOUT;
