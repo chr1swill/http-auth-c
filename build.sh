25f507d0ed240a299b661d8367722740111fe018
chr1swill <alexanderwilliams20003@gmail.com>
2025-07-03 17:17:37 -0400

    feat: update build so html to header is incremental - update login page content

--- build.sh
+++ build.sh
@@ -19,14 +19,56 @@ fi
 
 mkdir -p "$BIN"
 
+html_newer_than_header() {
+  html_file="$1"
+  case "$html_file" in
+    *.html);;
+    *)
+      echo "positional parameter 1 must be a *.html file" && \
+        exit 1
+    ;;
+  esac
+
+  header_file="$2"
+  case "$header_file" in
+    *.h);;
+    *)
+      echo "positional parameter 2 must be a *.h file" && \
+        exit 1
+    ;;
+  esac
+
+  html_file_time=$(stat -t --format="%Y" ${html_file} 2> /dev/null || "0")
+  header_file_time=$(stat -t --format="%Y" ${header_file} 2> /dev/null || "0")
+
+  if [ "$html_file_time" -eq "0" ];
+  then
+    echo "this should never happend" && exit 1
+  fi
+
+  if [ "$html_file_time" -gt "$header_file_time" ];
+  then
+    echo "1"
+  else
+    echo "0"
+  fi
+}
+
 for file in $(ls "$HTML");
 do
   case "$file" in
-    *.html) xxd -i ${HTML}/${file} > ${SRC}/${file}.h &
+    *.html) 
+      html_file_is_newer=$(html_newer_than_header "${HTML}/${file}" "${SRC}/${file}.h")
+      echo "$html_file_is_newer"
+      if [ "$html_file_is_newer" -eq "1" ];
+      then
+        xxd -i ${HTML}/${file} > ${SRC}/${file}.h &
+      fi
   esac
 done
 
-wait # making headers form html files
+# wait for all header to be build before compiling
+wait 
 
 gcc -o ${BIN}/main ${SRC}/main.c ${SRC}/picohttpparser.c ${SRC}/http_helpers.c ${CFLAGS}
 
